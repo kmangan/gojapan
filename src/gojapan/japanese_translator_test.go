2b0699d10eded59b74e7cf2f9a1385934f02f83d
kieranmangan <kieran.mangan@wowcher.co.uk>
2017-08-15 15:39:45 +0100

    Increase test scope

--- src/gojapan/japanese_translator_test.go
+++ src/gojapan/japanese_translator_test.go
@@ -5,9 +5,19 @@ import (
 	"testing"
 )
 
-func TestTranslate(t *testing.T) {
+func TestTranslateKatakana(t *testing.T) {
 	result := translate("happy")
 	assertEquals(t, result.japanese, "ハッピー")
+}
+
+func TestTranslateKatakanaTwoWords(t *testing.T) {
+	result := translate("panda box")
+	assertEquals(t, result.japanese, "パンダボックス")
+}
+
+func TestTranslateKanji(t *testing.T) {
+	result := translate("old")
+	assertEquals(t, result.japanese, "古い")
 }
 
 func assertEquals(t *testing.T, a interface{}, b interface{}) {
