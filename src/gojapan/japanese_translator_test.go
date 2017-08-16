3819898ac1bf5561e0829a412df75acc45bb26b1
kieranmangan <kieran.mangan@wowcher.co.uk>
2017-08-16 10:25:55 +0100

    Add test for two word kanji translation

--- src/gojapan/japanese_translator_test.go
+++ src/gojapan/japanese_translator_test.go
@@ -20,6 +20,11 @@ func TestTranslateKanji(t *testing.T) {
 	assertEquals(t, result.japanese, "古い")
 }
 
+func TestTranslateKanjiTwoWords(t *testing.T) {
+	result := translate("guardian angel")
+	assertEquals(t, result.japanese, "守護天使")
+}
+
 func assertEquals(t *testing.T, a interface{}, b interface{}) {
 	if a == b {
 		return
